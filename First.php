d361783369db1ecda740cb3c3c7e0939f5c8518c
AlexIndex <alex.indexstudio@yandex.ru>
2015-11-23 13:26:18 +0300

    ПХП загрузнул

---
+++ First.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Php Start</title>
+</head>
+<body>
+
+	<h1>
+		<?php 
+
+		echo "Hello Php World!!!!  <br> this is real cool theme   <br>
+		yeeeee";  //comment
+
+		
+		
+
+		?>
+	</h1>
+
+
+
+	<p>  
+		<?php 
+
+		$cool_var = "Php_variable а еще я ведь тута могу писать любой текст какой только захочу";
+
+		echo "$cool_var";
+
+
+
+		?>  
+
+
+	</p>
+
+
+
+	<br>
+	<br>
+	<br>
+
+
+
+
+
+
+
+</body>
+</html>
+
+
+
